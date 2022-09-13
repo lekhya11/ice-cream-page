a6cdace639f964f864cf2780c00a878edd0b463f
lekhya11 <lekhya.kondaveeti@gmail.com>
2022-09-13 08:32:04 +0000

    web page using style component

---
+++ src/styledComponents.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+export const Heading = styled.h1`
+  color: pink;
+  font-size: 30px;
+  font-family: 'Roboto';
+  font-style: italic;
+`
+
+export const Card = styled.div`
+  height: 110px;
+  width: 100px;
+  border-radius: 20px;
+  border: none;
+  background-color: white;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: center;
+  padding: 5px;
+`
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+export const CustomButton = styled.button`
+  padding: 10px;
+  font-size: 15px;
+  color: rosybrown;
+  border-radius: 5px;
+  border: none;
+  background-color: lavender;
+  margin: 20px;
+`
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0;
+`
